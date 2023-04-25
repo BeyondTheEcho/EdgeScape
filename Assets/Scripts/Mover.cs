fb7b82ce91862f23ac2a973a0825c600479849f2
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-04-25 02:16:28 -0400

    Added Click To Move

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,21 +6,32 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private Transform m_TargetPos;
-
+    private Transform m_TargetPos;
+    private Ray m_Ray;
     private NavMeshAgent m_Agent;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Agent = GetComponent<NavMeshAgent>();
-
-        m_Agent.destination = m_TargetPos.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            MoveTo();
+        }
+    }
+
+    private void MoveTo()
+    {
+        m_Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(m_Ray, out RaycastHit hit))
+        {
+            m_Agent.destination = hit.point;
+        }
     }
 }
