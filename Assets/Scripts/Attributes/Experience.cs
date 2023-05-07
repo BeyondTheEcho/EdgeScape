617c1b7410c51832f21b9c520a3d38319b0f779b
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-05-06 21:25:45 -0400

    Added XP Saving

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Attributes
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, IJsonSaveable
     {
         [SerializeField] private float m_ExperiencePoints = 0;
 
         public void GainExperience(float experience)
         {
             m_ExperiencePoints += experience;
+        }
+
+        public JToken CaptureAsJToken()
+        {
+            return JToken.FromObject(m_ExperiencePoints);
+        }
+
+        public void RestoreFromJToken(JToken state)
+        {
+            m_ExperiencePoints = state.ToObject<float>();
         }
     }
 
