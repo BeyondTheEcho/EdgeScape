0f33175209a91d7da268e13c6ba8374d38ab838f
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-05-18 17:52:59 -0400

    Added Hiding HealthBar on Conditions

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -8,10 +8,20 @@ namespace RPG.Attributes
     {
         [SerializeField] private RectTransform m_HealthBar;
         [SerializeField] private Health m_Health;
+        [SerializeField] private Canvas m_RootCanvas;
         
         void Update()
         {
-            m_HealthBar.localScale = new Vector3(m_Health.GetFraction(), 1, 1);
+            float healthFraction = m_Health.GetFraction();
+
+            if (Mathf.Approximately(healthFraction, 0f) || Mathf.Approximately(healthFraction, 1f))
+            {
+                m_RootCanvas.enabled = false;
+                return;
+            }
+
+            m_RootCanvas.enabled = true;
+            m_HealthBar.localScale = new Vector3(healthFraction, 1, 1);
         }
     }
 }
