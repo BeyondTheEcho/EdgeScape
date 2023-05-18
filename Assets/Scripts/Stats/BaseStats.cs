9cc75f98581df390ea37704d8e2cc77626becf70
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-05-18 04:27:29 -0400

    Added Event System / UI to Zone 2 + Removed Old Build

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -3,14 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using GameDevTV.Utils;
 using RPG.Attributes;
-using UnityEditorInternal;
 using UnityEngine;
 
 namespace RPG.Stats
 {
     public class BaseStats : MonoBehaviour
     {
-        [SerializeField][Range(1,99)] private int m_StartingLevel = 1;
+        [SerializeField] [Range(1,99)] private int m_StartingLevel = 1;
         [SerializeField] private CharacterClass m_CharacterClass;
         [SerializeField] private Progression m_Progression;
         [SerializeField] private GameObject m_LevelUpParticleEffect;
