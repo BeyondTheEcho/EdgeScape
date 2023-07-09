3f1eb7e5f3405bd4f600d1c005937ccd0bfe001c
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-07-09 18:22:23 -0400

    Added Dialogue Editor Functionality

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -8,13 +8,15 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        private Dialogue m_SelectedDialogue = null;
+
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
         }
 
-        [OnOpenAssetAttribute(1)]
+        [OnOpenAsset(1)]
         public static bool OnOpenDialogue(int instanceID, int line)
         {
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
@@ -25,6 +27,32 @@ namespace RPG.Dialogue.Editor
             }
 
             return false;
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChange;
+        }
+
+        private void OnSelectionChange()
+        {
+            if (Selection.activeObject is Dialogue dialogue) 
+            {
+                m_SelectedDialogue = dialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (m_SelectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected");
+            }
+            else
+            {
+                EditorGUILayout.LabelField(m_SelectedDialogue.name);
+            }
         }
     }
 }
